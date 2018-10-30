42487438523b77b06762f4c32deec5b77d25b895
Daniel Kraic <daniel.kraic@eset.sk>
2018-10-30 01:28:13 +0100

    Update app-c

--- app-c/main.c
+++ app-c/main.c
@@ -3,18 +3,14 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("hola\n");
     setlogmask (LOG_UPTO (LOG_NOTICE));
-
-    printf("started\n");
     openlog ("rsyslog-app-c", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
-    printf("opened\n");
-
+ 
     while (1) {
         syslog (LOG_ERR, "some c msg");
-        printf("msg send\n");
-        sleep(5);
+        sleep(3);
     }
 
     closelog ();
+    return 0;
 }
