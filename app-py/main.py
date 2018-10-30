a9231761b40f5db56835354e1c27ee093de3a267
Daniel Kraic <daniel.kraic@eset.sk>
2018-10-30 01:19:50 +0100

    Fix app-py

--- app-py/main.py
+++ app-py/main.py
@@ -1,26 +1,34 @@
+import datetime
+import socket
 import time
 import logging
 import logging.handlers
+
+# inspired by https://github.com/EasyPost/syslog-rfc5424-formatter/
+class MyFormatter(logging.Formatter, object):
+    def __init__(self, *args, **kwargs):
+        return super(MyFormatter, self).__init__(*args, **kwargs)
+
+    def format(self, record):
+        record.__dict__['isotime'] = datetime.datetime.fromtimestamp(record.created).strftime(format='%b %d %H:%M:%S')
+
+        header = '{isotime} {name}[{process}]: '.format(
+            **record.__dict__
+        )
+        
+        return header + super(MyFormatter, self).format(record)
 
 def main():
     logger = logging.getLogger('rsyslog-app-py')
     logger.setLevel(logging.INFO)
 
-    #add handler to the logger
-    handler = logging.handlers.SysLogHandler('/var/run/rsyslog/dev/log')
-
-    #add formatter to the handler
-    # formatter = logging.Formatter('Python: { "loggerName":"%(name)s", "timestamp":"%(asctime)s", "pathName":"%(pathname)s", "logRecordCreationTime":"%(created)f", "functionName":"%(funcName)s", "levelNo":"%(levelno)s", "lineNo":"%(lineno)d", "time":"%(msecs)d", "levelName":"%(levelname)s", "message":"%(message)s"}')
-    formatter = logging.Formatter('%(filename)s: %(funcName)s: %(message)s')
-
-    handler.formatter = formatter
+    handler = logging.handlers.SysLogHandler('/dev/log')
+    handler.setFormatter(MyFormatter())
     logger.addHandler(handler)
-
-    logger.info("Test Message")
 
     while (True):
         logger.info('some py msg')
-        time.sleep(5)
+        time.sleep(3)
 
 if __name__ == '__main__':
     main()
